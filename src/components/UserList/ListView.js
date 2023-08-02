3f33b9bbab0ccc6b618942ca091419d7d4986033
AdamBurawski <a.burawski.post@gmail.com>
2023-08-02 16:07:24 +0200

    layout fixed FakeUsers setError  v.2.4

--- src/components/UserList/ListView.js
+++ src/components/UserList/ListView.js
@@ -3,19 +3,23 @@ import { ListElement } from "./ListElement";
 
 export const ListView = (props) => {
   const [users, setUsers] = useState(props.list);
+  const [error, setError] = useState(null);
 
   const changeUser = () => {
     (async () => {
       try {
-        // setError(null);
+        setError(null);
         const res = await fetch("https://randomuser.me/api/");
         const data = await res.json();
         setUsers(data.results); // Update the state with the fetched users list
       } catch (e) {
-        // setError("Błąd wczytywania");
+        setError("Błąd wczytywania");
       }
     })();
   };
+  if (error) {
+    return <p>{error}</p>;
+  }
 
   const list = [...users]
     .sort((a, b) => a.username.localeCompare(b.username))
