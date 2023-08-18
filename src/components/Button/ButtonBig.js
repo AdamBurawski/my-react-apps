a7421d693255d9d84f39c15852367987af089d6e
AdamBurawski <a.burawski.post@gmail.com>
2023-08-18 17:22:58 +0200

    app updated project v.3.0 (finished)

--- src/components/Button/ButtonBig.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Button } from "./Button";
-
-export const ButtonBig = (props) => {
-  return (
-    <Button style={{ padding: `${props.value}` }}> {props.children}</Button>
-  );
-};
